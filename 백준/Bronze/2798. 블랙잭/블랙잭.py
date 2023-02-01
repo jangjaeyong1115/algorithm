63d40d204aed4553cea0c7ea8bc6604adf6d26ae
jaeyong <121473942+jangjaeyong1115@users.noreply.github.com>
2023-02-01 17:09:25 +0900

    [Bronze II] Title: 블랙잭, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+n, m = map(int,input().split())
+nums = sorted(list(map(int,input().split())), reverse = True)
+
+m_n = 0
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            sol = nums[i] + nums[j] + nums[k]
+            if sol <= m:
+                if m_n < sol: m_n = sol
+                break
+print(m_n)
